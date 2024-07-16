a10eb4056b18d6afb22a4c5fd8e1b57270fbb4b6
mj-life-is-once <minjoolisa.cho@gmail.com>
2024-07-16 19:53:13 +0100

    task_filter London

--- dags/transform_historical_weather.py
+++ dags/transform_historical_weather.py
@@ -104,12 +104,26 @@ def transform_historical_weather():
         ).df()
 
         ####### YOUR TRANSFORMATION ##########
-        # input_df["time"] = pd.to_datetime(input_df["time"], format="%Y-%m-%d")
-        # output_df = input_df[input_df["time"].dt.year == birthyear]
-        # # remove lat, log
-        # output_df = output_df.drop(columns=["lat", "lon"])
-        output_df = input_df
+        input_df["time"] = pd.to_datetime(input_df["time"])
+        output_df = input_df[
+            [input_df["time"].dt.year == birthyear] and [input_df["city"] == uv.MY_CITY]
+        ]
 
+        # remove lat, log
+        output_df = output_df.drop(columns=["lat", "long"])
+        # rename columns
+        output_df = output_df.rename(
+            columns={
+                "time": "Time",
+                "temperature_2m_max": "Max Temperature",
+                "city": "City",
+            }
+        )
+        # change datetime format
+        output_df["Time"] = output_df["Time"].dt.strftime("%Y-%m-%d")
+
+        # drop previous table
+        cursor.sql(f"DROP TABLE IF EXISTS {output_table_name}")
         # saving the output_df to a new table
         cursor.sql(
             f"CREATE TABLE IF NOT EXISTS {output_table_name} AS SELECT * FROM output_df"
